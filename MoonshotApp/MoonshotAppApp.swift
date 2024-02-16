2c37264f5133e839e77cd987a6a893f459fe0831
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-02-16 15:10:14 -0300

    Initial Commit

---
+++ MoonshotApp/MoonshotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotAppApp.swift
+//  MoonshotApp
+//
+//  Created by Bruno Oliveira on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
